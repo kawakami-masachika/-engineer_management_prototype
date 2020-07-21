283fca7d7cacf5deb38b29f6ccaf87a9c2144367
kawakami-masachika <supercell1023koeda@gmail.com>
2020-07-21 10:48:17 +0900

    フルネーム表示用処理の実装 #13

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,19 @@ class Employee < ApplicationRecord
   has_many :licenses
   has_many :mst_skills, :through => :employee_siklls
   belongs_to :mst_gender
+
+  attr_accessor :name, :name_kana
+
+  CONST_HALF_SPACE = " "
+  CONST_HALF_SPACE.freeze
+
+  def create_full_name(last_name, first_name)
+    binding.pry
+    self.name = last_name + CONST_HALF_SPACE + first_name
+    return self.name
+  end
+
+  def create_full_name_kana(last_name_kana, first_name_kana)
+    self.name_kana = last_name_kana + CONST_HALF_SPACE + first_name_kana
+  end
 end
