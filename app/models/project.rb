1041cdface206d36f8cb8866e8b6e2c8bd2d0785
kawakami-masachika <supercell1023koeda@gmail.com>
2020-11-27 21:57:17 +0900

    バリデーション・アソシエーション修正

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,20 @@
 class Project < ApplicationRecord
-  # 関連付け
+  attr_accessor :validate_flg, :join_date, :leave_date
+
+  validate_flg = false
+  VALIDATE_MEMBERS_FORMAT = /\A[0-9]+\z/
+  JOIN_FLG = 0
+  LEAVE_FLG = 1
+  CONST_JOIN_DATE = "join_date"
+  CONST_LEAVE_DATE = "leave_date"
+
+  #モデル アソシエーション
+  has_many :employees, through: :project_members
+  has_many :project_members, dependent: :destroy
   has_many :project_phases, dependent: :destroy
+  has_many :mst_phases, through: :project_phases
   has_many :adopt_technologies, dependent: :destroy
   has_many :mst_skills, through: :adopt_technologies
-  has_many :project_members, dependent: :destroy
-  has_many :employees, through: :project_members
 
   belongs_to :mst_industry
 
